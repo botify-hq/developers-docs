23a4e48445613a29af25b68d7e473265e78e9b74
Pablo Leano <pleasedontbelong@gmail.com>
2016-01-26 15:54:43 +0100

    Add renderer for django medusa

--- api_docs/renderers.py
+++ api_docs/renderers.py
@@ -1,10 +1,27 @@
 from django_medusa.renderers import StaticSiteRenderer
+from .index import PAGES
+from utils import reverse_page
 
 
-class HomeRenderer(StaticSiteRenderer):
+class APIMdDocsRenderer(StaticSiteRenderer):
     def get_paths(self):
-        return frozenset([
-            "/",
-        ])
+        api_md_pages_paths = [reverse_page(page['url_name']) for page in PAGES]
+        return frozenset(api_md_pages_paths)
 
-renderers = [HomeRenderer, ]
+
+class StaticDjangoRenderer(StaticSiteRenderer):
+
+    EXCLUDED_PATTERNS = [
+        'md-page'
+    ]
+
+    def get_paths(self):
+        from .urls import urlpatterns
+        paths = []
+        for pattern in urlpatterns:
+            if pattern.name not in self.EXCLUDED_PATTERNS:
+                paths.append(reverse_page(pattern.name))
+        return frozenset(paths)
+
+
+renderers = [APIMdDocsRenderer, StaticDjangoRenderer]
