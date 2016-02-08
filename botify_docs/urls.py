e5eec5967c654eae317456b52684d39e52273149
kamalbennani <kamalbouchboy1@hotmail.fr>
2016-02-08 12:33:57 +0100

    Add homepage draft

--- botify_docs/urls.py
+++ botify_docs/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
+
+from .views import HomepageView
+
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api', include('api_docs.urls'))
+    url(r'^api', include('api_docs.urls')),
+    url(r'', HomepageView.as_view(), name='homepage'),
 )
