386f8c99600e3caa2f34b472ed8e75dea7b445d4
kamalbennani <kamalbouchboy1@hotmail.fr>
2016-02-10 10:04:53 +0100

    add commun page

--- botify_docs/views.py
+++ botify_docs/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomepageView(TemplateView):
     template_name = "homepage.html"
+
+class CommunView(TemplateView):
+    template_name = "commun.html"
