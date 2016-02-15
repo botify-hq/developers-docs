ab538cad0aab5f7b33c83078c986c08720d3de9f
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2016-02-15 09:41:32 +0100

    Change settings for production

--- botify_docs/settings/deploy_prod.py
+++ botify_docs/settings/deploy_prod.py
@@ -4,7 +4,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['testserver']
 
-STATIC_URL = 'https://d240r91knyqgcy.cloudfront.net/'
+STATIC_URL = 'https://developers.botify.com/'
 
 STATICFILES_STORAGE = 'botify_docs.storage.StaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'botify_docs.storage.MediaFilesStorage'
@@ -12,4 +12,4 @@ DEFAULT_FILE_STORAGE = 'botify_docs.storage.MediaFilesStorage'
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', None)
 AWS_ACCESS_KEY = AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', None)
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', None)
+AWS_STORAGE_BUCKET_NAME = 'com.botify.saas.production.developer-docs'
