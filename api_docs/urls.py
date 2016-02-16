fdd47434dcffcfeb758b0f14d96ba9a308d71a69
Pablo Leano <pleasedontbelong@gmail.com>
2016-02-16 18:42:43 +0100

    Fix dynamic trailing slash on urls

--- api_docs/urls.py
+++ api_docs/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns(
     '',
     url(r'api/urls-datamodel', views.DatamodelView.as_view(), name='urls-datamodel'),
     url(r'api/reference', views.SwaggerUiView.as_view(), name='reference'),
-    url(r'(?P<path>.+)/', views.MarkdownPageView.as_view(), name='md-page'),
+    url(r'(?P<path>.+)/$', views.MarkdownPageView.as_view(), name='md-page'),
 )
