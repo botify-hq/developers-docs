ea46d1b49be9e7a9588e9ea17e0d80bd1be4f5cd
Adèle Delamarche <adele.delamarche@gmail.com>
2016-03-15 18:20:53 +0100

    Implement redirects

--- api_docs/urls.py
+++ api_docs/urls.py
@@ -1,10 +1,21 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from . import views
 
-urlpatterns = patterns(
-    '',
-    url(r'api/reference/$', views.SwaggerUiView.as_view(), name='reference'),
-    url(r'api/analysis/urls-datamodel/$', views.DatamodelView.as_view(), name='analysis-urls-datamodel'),
-    url(r'404/$', views.NotFoundView.as_view(), name='404'),
-    url(r'(?P<path>.+)/$', views.MarkdownPageView.as_view(), name='md-page'),
-)
+
+urlpatterns = [
+    url(r'api/reference/$',
+        views.SwaggerUiView.as_view(),
+        name='reference'),
+
+    url(r'api/analysis/urls-datamodel/$',
+        views.DatamodelView.as_view(),
+        name='analysis-urls-datamodel'),
+
+    url(r'404/$',
+        views.NotFoundView.as_view(),
+        name='404'),
+
+    url(r'(?P<path>.+)/$',
+        views.MarkdownPageView.as_view(),
+        name='md-page'),
+]
