212be6bc51ee585d4394a146240283f11574f236
Adèle Delamarche <adele.delamarche@gmail.com>
2016-03-22 18:24:41 +0100

    Add comments for variable ALLOWED_HOSTS in settins

--- botify_docs/settings/staging.py
+++ botify_docs/settings/staging.py
@@ -2,6 +2,8 @@ from .base import *  # NOQA
 
 DEBUG = False
 
+# This variable is needed otherwise django-medusa
+# renderers don't work (403 with DEBUG=False):
 ALLOWED_HOSTS = ['testserver']
 
 STATIC_URL = '/staticfiles/'
